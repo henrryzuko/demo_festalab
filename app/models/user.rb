cdfcea599a804d4a2a2e83fffef784afd2ffe175
henrryzuko <henrimend1999@gmail.com>
2023-10-12 18:26:18 -0300

    Teste finalizado, aparentemente

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-    validates :name, presence: { message: 'obrigatório' }
+    validates :name, presence: { message: 'Obrigatório!' }
     validates :phone, phone: { message: 'Inválido!' }
     validates :email, email: { message: 'Inválido!' }
     validates :cpf, cpf: { message: 'Inválido!' }
+
+    def self.search(query)
+        query = "%#{query}%"
+        where("name like ? or email like ? or phone like ? or cpf like ?", query, query, query, query)
+    end
 end
